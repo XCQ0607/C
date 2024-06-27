87aae7e222cb8599c369ac2b9c25e081819f5ce1
XCQ0607 <xcq0607@outlook.com>
2024-06-27 08:35:07 +0000

    demo

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "go.c"
+
+//2869
+// int main() {
+//     int array[] = {3,1,5,4,2};
+//     int result = minOperations(array, sizeof(array)/sizeof(array[0]), 2);
+//     printf("%d", result);
+//     return 0;
+// }
+
+//2760
+// int main() {
+//     int nums[] = {3,2,5,4};
+//     int threshold = 5;
+//     int result = longestAlternatingSubarray(nums, sizeof(nums)/sizeof(nums[0]), threshold);
+//     printf("%d", result);
+//     return 0;
+// }
+
+
+//2784
+// int main() {
+//     int nums[] = {2, 1, 3};
+//     bool result = isGood(nums, sizeof(nums) / sizeof(nums[0]));
+//     if (result) {
+//         printf("数组 nums 是有效的！\n");
+//     } else {
+//         printf("数组 nums 无效。\n");
+//     }
+    
+//     return 0;
+// }
